e5418ecd1f8257b3d660906329f73d21f97f2ce5
Domenico <menico.c90@gmail.com>
2024-08-12 10:50:40 +0200

    feat: add form filter

--- src/app/movies-catalog/movies-catalog.component.ts
+++ src/app/movies-catalog/movies-catalog.component.ts
@@ -6,25 +6,40 @@ import { Movie } from '../shared/models/movie.model';
 import { CommonModule, CurrencyPipe } from '@angular/common';
 import { BudgetPipe } from '../shared/pipes/budget.pipe';
 import { DurationPipe } from '../shared/pipes/duration.pipe';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-movies-catalog',
   standalone: true,
-  imports: [HttpClientModule, CommonModule, BudgetPipe, DurationPipe],
+  imports: [HttpClientModule, CommonModule, BudgetPipe, DurationPipe, ReactiveFormsModule],
   providers: [MoviesService, CurrencyPipe],
   templateUrl: './movies-catalog.component.html',
   styleUrl: './movies-catalog.component.css'
 })
 export class MoviesCatalogComponent implements OnInit{
-
   movies$!: Observable<Movie[]>;
+  formFilter!: FormGroup;
 
   private moviesService = inject(MoviesService);
+  private formBuilder = inject(FormBuilder);
 
   ngOnInit(): void {
     this.movies$ = this.moviesService.getMovies();
+
+    this.setForm();
   }
 
+  setForm() {
+    this.formFilter = this.formBuilder.group({
+      title: [''],
+      release_year: ['']
+    });
+
+    this.formFilter.valueChanges.subscribe(form => {
+      this.movies$ = this.moviesService.getMovies(form.title, form.release_year);
+    });
+  }
+  
   onDetails(id: string){
     console.log(id);
     
